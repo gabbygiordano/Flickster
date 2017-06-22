72e006ad68fd65c8c385c7c9d130af9b775b859c
Gabby Giordano <gabby.giordano@gmail.com>
2017-06-22 14:06:00 -0700

    updated to include landscape

--- app/src/main/java/com/example/gabbygiordano/flixster/models/MovieData.java
+++ app/src/main/java/com/example/gabbygiordano/flixster/models/MovieData.java
@@ -12,12 +12,14 @@ public class MovieData {
     private String title;
     private String overview;
     private String posterPath; //only the path
+    private String backdropPath;
 
     //initialize from JSON data
     public MovieData(JSONObject object) throws JSONException {
         title = object.getString("title");
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
+        backdropPath = object.getString("backdrop_path");
     }
 
     public String getTitle() {
@@ -30,5 +32,9 @@ public class MovieData {
 
     public String getPosterPath() {
         return posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
     }
 }
